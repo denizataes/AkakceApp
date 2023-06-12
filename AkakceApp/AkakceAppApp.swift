6ef2d585663a86ebcc979444148b2862ec5c85c3
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2023-06-12 21:44:13 +0300

    Initial Commit

---
+++ AkakceApp/AkakceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkakceAppApp.swift
+//  AkakceApp
+//
+//  Created by Deniz Ata Eş on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AkakceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
